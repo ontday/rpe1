4b022b35b314e2720eb42152263ddf43ab46bfed
frj <100>
2019-07-18 22:26:00 +0800

    提交到本地

---
+++ helloWorld/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //提交
+        Map map = new HashMap();
+        int a = 1;
+        int i  = 100*1000*1000;
+        long l = System.currentTimeMillis();
+        for (int i1 = 0; i1 < i; i1++) {
+            List<Integer> list = new ArrayList<>();
+            list.add(1);
+         }
+        long l1 = System.currentTimeMillis();
+        System.out.println(l1-l);
+    }
+}
